8724884946296048b90c83ed027544ba2cafb689
dechilds1@gmail.com <dechilds1@gmail.com>
2021-08-30 13:31:54 +0200

    Solution

--- 20210830.js
+++ 20210830.js
@@ -1,2 +1,19 @@
 //Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
+var moveZeros = function (arr) {
+    let newArr = [];
+    let zeroArr = [];
+    for(let i=0; i<arr.length; i++){
+      if(arr[i]===0) zeroArr.push(arr[i])
+      else newArr.push(arr[i]);
+    }
+    return newArr.concat(zeroArr);
+  }
+
+  /*
+  Top-rated solution:
+  
+  var moveZeros = function (arr) {
+    return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+  }
+  */
