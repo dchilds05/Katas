a69f08bb2e34c237b0cd8d753d965704ae425384
dechilds1@gmail.com <dechilds1@gmail.com>
2021-10-01 16:30:34 +0200

    solution

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -25,7 +25,7 @@ function likes(names) {
       case 3:
         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
         break;
-      case 4:
+      default:
         return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
         break;
   }
