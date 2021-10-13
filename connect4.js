5861ece7948dcf1f8d555c1e8c05abe7339acd6e
Daniel Childs <dechilds1@gmail.com>
2021-10-13 13:23:12 +0200

    function horizontal checker

--- connect4.js
+++ connect4.js
@@ -7,7 +7,7 @@ The game consists of a grid (7 columns and 6 rows) and two players that take tur
 
 Your task is to create a Class called Connect4 that has a method called play which takes one argument for the column where the player is going to place their disc.
 Rules
-If a player successfully has 4 discs horizontally, vertically or diagonally then you should return "Player n wins!” where n is the current player either 1 or 2.
+If a player successfully has 4 discs horizontally or vertically then you should return "Player n wins!” where n is the current player either 1 or 2.
 
 If a player attempts to place a disc in a column that is full then you should return ”Column full!” and the next move must be taken by the same player.
 
@@ -37,23 +37,36 @@ class Connect4{
     }
     
     play(col) {
-      if(this.gameFinished === true) return "Game has finshed!";
+      //CHECK FOR FINISHED GAME
+      if(this.gameFinished === true) return "Game has finished!";
   
+      //CHECK FOR FULL COLUMN, PLAYER TAKES SPACE ON BOARD
+      let rowMarker = 0;
       for(let i = 5; i >= 0; i--){
         if(i === 0 && this.board[i][col] !== 0) return "Column full!"
         if(this.board[i][col] === 0) {
           this.board[i][col] = this.n;
+          rowMarker = i;
           break;
         }
       }
   
-      let [checker1, checker2] = [0,0];
+      //CHECK FOR WINNER
+      let [vChecker1, vChecker2, hChecker1, hChecker2] = [0,0,0,0];
       for (let i = 0; i < 6; i++){
-        if(this.board[i][col] === 1) checker1++;
-        if(this.board[i][col] === 2) checker2++;
+        if(this.board[i][col] === 1) vChecker1++;
+        if(this.board[i][col] === 2) vChecker2++;
       }
-      if (checker1 >= 4 || checker2 >= 4) this.gameFinished = true;
+      for (let i = 0; i < 7; i++){
+          if(this.board[rowMarker][i] === 1) hChecker1++;
+          if(this.board[rowMarker][i] === 2) hChecker2++;
+      }
+      if (vChecker1 >= 4 || vChecker2 >= 4 || hChecker1 >= 4 || hChecker2 >= 4) {
+          this.gameFinished = true;
+          return `Player ${this.n} wins!`;
+      }
   
+      //ADJUST CURRENT AND NEXT PLAYER
       if (this.n === 1) {
         this.n = 2;
         this.hadTurn = "1";
@@ -63,7 +76,7 @@ class Connect4{
         this.hadTurn = "2";
       }
   
-      if (this.gameFinished === true) return `Player ${this.hadTurn} wins!`;
+      //RETURN WHO JUST HAD TURN
       return `Player ${this.hadTurn} has a turn`;
     };
   };
