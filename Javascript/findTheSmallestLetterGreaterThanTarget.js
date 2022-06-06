704e8b14417468245388b10c25d3993267372112
Daniel Childs <dechilds1@gmail.com>
2022-06-06 00:37:53 -0700

    solution

---
+++ Javascript/findTheSmallestLetterGreaterThanTarget.js
@@ -0,0 +1,34 @@
+/*
+Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+
+Note that the letters wrap around.
+
+For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+ 
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Example 3:
+
+Input: letters = ["c","f","j"], target = "d"
+Output: "f" 
+*/
+
+var nextGreatestLetter = function(letters, target) {
+    let lib = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    
+    let index = lib.indexOf(target);
+    
+    for(let i = 0; i < letters.length; i++){
+        if(lib.includes(letters[i],index+1)) return letters[i];
+    }
+    
+    return letters[0];
+};
+
